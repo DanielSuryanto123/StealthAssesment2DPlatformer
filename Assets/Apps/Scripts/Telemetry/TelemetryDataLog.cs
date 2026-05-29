9b333cd40fa5b67e5f03a3bdf8f72f07da94170b
Francisco M.P. <9068592+fmpanggabean@users.noreply.github.com>
2026-05-29 18:12:53 +0700

    penambahan fitur: pencatatan telemetry

---
+++ Assets/Apps/Scripts/Telemetry/TelemetryDataLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoLab.Telemetry
+{
+    /// <summary>
+    /// class ini digunakan sebagai representasi/data model yntuk disimpan dalam bentuk JSON
+    /// data didapat dari convert isi class TelemetryData, terutama eventType dan playerID dari enum ke string
+    /// </summary>
+    [Serializable]
+    public class TelemetryDataLog
+    {
+        public string timestamp;
+        public string eventType;
+        public string playerId;
+        public Position playerPosition;
+        public Position partnerPosition;
+        public bool isTrapped;
+
+        public TelemetryDataLog(string timestamp, EventType eventType, PlayerID playerId, Position playerPosition, Position partnerPosition, bool isTrapped)
+        {
+            this.timestamp = timestamp;
+            this.eventType = eventType.ToString();
+            this.playerId = playerId.ToString();
+            this.playerPosition = playerPosition;
+            this.partnerPosition = partnerPosition;
+            this.isTrapped = isTrapped;
+        }
+    }
+}

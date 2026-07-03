9a06697fda2674c4b7c4543f55f3ed74efb53355
DanielSuryanto123 <danielsuryanto901@gmail.com>
2026-07-03 18:10:43 +0700

    2

--- Assets/Apps/Scripts/Gameplay/EndPortal.cs
+++ Assets/Apps/Scripts/Gameplay/EndPortal.cs
@@ -3,50 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class EndPortal : MonoBehaviour
 {
-    private bool redInside;
-    private bool blueInside;
+    private bool player1Inside;
+    private bool player2Inside;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("RedPlayer"))
-        {
-            redInside = true;
-        }
+        if (other.CompareTag("Player1"))
+            player1Inside = true;
 
-        if(other.CompareTag("BluePlayer"))
-        {
-            blueInside = true;
-        }
+        if (other.CompareTag("Player2"))
+            player2Inside = true;
 
         CheckPortal();
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.CompareTag("RedPlayer"))
-        {
-            redInside = false;
-        }
+        if (other.CompareTag("Player1"))
+            player1Inside = false;
 
-        if(other.CompareTag("BluePlayer"))
-        {
-            blueInside = false;
-        }
+        if (other.CompareTag("Player2"))
+            player2Inside = false;
     }
 
     private void CheckPortal()
     {
-        if(redInside && blueInside)
+        if (player1Inside && player2Inside)
         {
-            LoadNextLevel();
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene + 1);
         }
-    }
-
-    private void LoadNextLevel()
-    {
-        int currentScene =
-            SceneManager.GetActiveScene().buildIndex;
-
-        SceneManager.LoadScene(currentScene + 1);
     }
 }
